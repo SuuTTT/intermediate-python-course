c49c43ba57d1dafa80e4cbe65104cd391956c90f
SuuTTT <48884113+SuuTTT@users.noreply.github.com>
2021-08-27 16:07:57 +0800

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,16 @@
 import random
 def main():
   sum=0
-  cnt=2
+  cnt=int(input('how many time would you like to roll?'))
+  mx=int(input('how many side are the dice?'))
   for i in range(0,cnt):
-    roll=random.randint(1,6)
-    print(f'You rolled a {roll}')
+    roll=random.randint(1,mx)
+    if roll==1:
+      print(f'You rolled a {roll}!fail')
+    elif roll ==mx:
+      print(f'You rolled a {roll}!success')
+    else:
+      print(f'You rolled a {roll}')
     sum+=roll
   print(f'You rolled a total of {sum}')
 
